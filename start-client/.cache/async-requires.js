43d211e04c58bae3521b9c63cf77fc9f18de1202
diego <diego.junior10@gmail.com>
2019-09-03 14:11:57 -0300

    client

---
+++ start-client/.cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": () => import("/home/diego/developer/source-projects/start.spring.io-master/start-client/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/home/diego/developer/source-projects/start.spring.io-master/start-client/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
